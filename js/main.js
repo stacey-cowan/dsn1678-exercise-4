57c26f38b3afe7152e87367af420e6fe075ae3fe
stacey-cowan <cowan.stacey.sc@gmail.com>
2014-10-07 09:15:04 -0400

    added dipper section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $win.on('scroll', function () {
     var scrollPos = $win.scrollTop();
     
     $sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
-    $sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
+    $sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
 
 
