a9a77655e5219709a5eab8774c5cfec548f0f585
stacey-cowan <cowan.stacey.sc@gmail.com>
2014-10-07 08:36:36 -0400

    Added rotation to sun

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var $win = $(window)
+var $sun = $('.sun');
 
 $win.on('scroll', function () {
     var scrollPos = $win.scrollTop();
-    console.log(scrollPos); 
+    
+    $sun.css('transform', 'rotate(' + scrollPos + 'deg)');
 });
+
+
 
