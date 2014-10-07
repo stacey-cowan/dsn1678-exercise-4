298ed094105c8b087b17c485865cf4135888304b
stacey-cowan <cowan.stacey.sc@gmail.com>
2014-10-07 08:53:36 -0400

    Added parallax scrolling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-var $win = $(window)
+var $win = $(window);
 var $sun = $('.sun');
+var $sunSection = $('.sun-section');
 
 $win.on('scroll', function () {
     var scrollPos = $win.scrollTop();
     
     $sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+    $sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
 
 
